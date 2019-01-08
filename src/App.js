cf49a6544d120a7331284360a8c8971d816ce750
Meyers <tmeyer12@us.chs.net>
2019-01-08 12:34:30 -0600

    Build

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
-
+import '../public/fadein.js';
 import './App.css';
-
+import WOW from "wowjs";
 class App extends Component {
     constructor(props) {
         super(props);
@@ -21,7 +21,12 @@ class App extends Component {
         this.setState({
             isLoaded: true,
         })
+
+        const wow = new WOW.WOW();
+        wow.init();
+        wow.sync();
     }
+ 
 
     onStateChange = event => {
         this.setState({ State: event.target.value});
@@ -34,12 +39,12 @@ class App extends Component {
     updateCity = (e,items) => {
         const  {City}  = this.state;
         const  {State}  = this.state;
-        event.preventDefault();
+      
         console.log(City);
         console.log(State);
         
         //Your API weatherbit key here
-        const API_KEY = "YOUR API KEY HERE";
+        const API_KEY = "b6a7024fce944c3a8f969cc60b3c0141";
         
         const API_CALL = `https://api.weatherbit.io/v2.0/current?city=${City},${State}&key=${API_KEY}`;
         // const API_CALL2 = `http://api.weatherbit.io/v2.0/forecast/daily?city=${City},${State}&key=${API_KEY}`; 16 day forecast
@@ -63,11 +68,21 @@ class App extends Component {
             else if(this.state.condition === "Fog"){
                 document.getElementById("imageContainer").innerHTML="<img src='Fog.jpg' />";
             }
-            else if(this.state.condition === "Broken clouds" || this.state.condition === "Overcast clouds"){
-                document.getElementById("imageContainer").innerHTML="<img src='overcast.jpg' />";
+            else if(this.state.condition === "Broken clouds" ||
+                    this.state.condition ===  "Overcast clouds" ||
+                    this.state.condition === "Scattered clouds" ||
+                    this.state.condition === "Few clouds"
+                    ){
+
+                    document.getElementById("imageContainer").innerHTML="<img src='overcast.jpg' />";
             }
-            else{
-                document.getElementById("imageContainer").innerHTML=null; 
+            else if(this.state.condition === "Heavy rain" ||
+                    this.state.condition === "Light rain" ||
+                    this.state.condition === "Moderate rain")
+                    
+                    {
+
+                document.getElementById("imageContainer").innerHTML="<img src='heavyrain.jpg' />";
             }
         });
 
@@ -79,35 +94,39 @@ class App extends Component {
             return <div>Data Loading.. Please Wait...</div>;
         }
         return (
-            <div className="App">
-                <h1>What's your Weather?</h1>
-                <div className="app-container">
+            <div className="App wow fadeInDownBig">
+                <h1 className="title">What's your Weather?</h1>
+                <div className="app-container wow rotateIn" data-wow-delay="1.2s">
                   <div className="weather-container">
                   
                     <div className="weather-child temp">{this.state.cityName} {this.state.stateName}</div>  
                     <div className="weather-child temp">{this.state.temp}</div>
                     <div className="weather-child">{this.state.condition}</div> 
                     <div className="weather-child temp">{this.state.wind}</div> 
-                    <div id="imageContainer" > </div>
+                   
+                    <div id="imageContainer" ></div>
+
                   </div>
 
 
                   <div className="wrap">
-                  <form onSubmit={this.onSubmit}>
+               
                   <h1 className="string">Enter City</h1>
                     <input className="input2" onChange={this.onCityChange}  type="text"  id="city" placeholder="City" name="City" />
                   <h1 className="string">Enter State</h1>
                     <input className="input2" onChange={this.onStateChange} type="text" id="state" placeholder="State" name="State" />
                   
                     <br/>
-                    <button className="submit-button" onClick={this.updateCity}>Submit</button>
+                    <button className="submit-button wow fadeInDownBig" data-wow-delay="1.5s" onClick={this.updateCity}>Submit</button>
                     
-                  </form>
+                 
                  </div>
                  {/* End Wrap Div */}
                 </div>
             </div>
         );
+
+       
     }
 }
 export default App;
